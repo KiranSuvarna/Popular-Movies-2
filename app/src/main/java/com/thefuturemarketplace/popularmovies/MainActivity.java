0fef5b86d3b33f1a8ea3ca9e33d7f248e6a3f601
imKiran <kiransuvarna19@gmail.com>
2017-10-14 15:18:33 +0530

    re-submission code

--- app/src/main/java/com/thefuturemarketplace/popularmovies/MainActivity.java
+++ app/src/main/java/com/thefuturemarketplace/popularmovies/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Add menu items
         mMenu.add(Menu.NONE, // No group
-                R.string.pref_sort_pop_desc_key, // ID
+                R.string.pref_sort_toprated_key, // ID
                 Menu.NONE, // Sort order: not relevant
                 null) // No text to display
                 .setVisible(false)
@@ -79,7 +79,9 @@ public class MainActivity extends AppCompatActivity {
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
 
         // Same settings as the one above
-        mMenu.add(Menu.NONE, R.string.pref_sort_vote_avg_desc_key, Menu.NONE, null)
+        mMenu.add(Menu.NONE,
+                R.string.pref_sort_popular_key,
+                Menu.NONE, null)
                 .setVisible(false)
                 .setIcon(R.drawable.people3x)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
@@ -92,13 +94,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.string.pref_sort_pop_desc_key:
-                updateSharedPrefs(getString(R.string.tmdb_sort_pop_desc));
+            case R.string.pref_sort_popular_key:
+                updateSharedPrefs(getString(R.string.tmdb_sort_popular));
                 updateMenu();
                 getMoviesFromTMDb(getSortMethod());
                 return true;
-            case R.string.pref_sort_vote_avg_desc_key:
-                updateSharedPrefs(getString(R.string.tmdb_sort_vote_avg_desc));
+            case R.string.pref_sort_toprated_key:
+                updateSharedPrefs(getString(R.string.tmdb_sort_toprated));
                 updateMenu();
                 getMoviesFromTMDb(getSortMethod());
                 return true;
@@ -111,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
     private void getMoviesFromTMDb(String sortMethod) {
         if (isNetworkAvailable()) {
             // Key needed to get data from TMDb
-            String apiKey = getString(R.string.key_themoviedb);
+            String apiKey = BuildConfig.API_KEY;
 
             // Listener for when AsyncTask is ready to update UI
             OnTaskCompleted taskCompleted = new OnTaskCompleted() {
@@ -140,18 +142,18 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
         return prefs.getString(getString(R.string.pref_sort_method_key),
-                getString(R.string.tmdb_sort_pop_desc));
+                getString(R.string.tmdb_sort_popular));
     }
 
     private void updateMenu() {
         String sortMethod = getSortMethod();
 
-        if (sortMethod.equals(getString(R.string.tmdb_sort_pop_desc))) {
-            mMenu.findItem(R.string.pref_sort_pop_desc_key).setVisible(false);
-            mMenu.findItem(R.string.pref_sort_vote_avg_desc_key).setVisible(true);
+        if (sortMethod.equals(getString(R.string.tmdb_sort_popular))) {
+            mMenu.findItem(R.string.pref_sort_popular_key).setVisible(false);
+            mMenu.findItem(R.string.pref_sort_toprated_key).setVisible(true);
         } else {
-            mMenu.findItem(R.string.pref_sort_vote_avg_desc_key).setVisible(false);
-            mMenu.findItem(R.string.pref_sort_pop_desc_key).setVisible(true);
+            mMenu.findItem(R.string.pref_sort_toprated_key).setVisible(false);
+            mMenu.findItem(R.string.pref_sort_popular_key).setVisible(true);
         }
     }
 
@@ -162,160 +164,5 @@ public class MainActivity extends AppCompatActivity {
         editor.apply();
     }
 
-    public class FetchMovieAsyncTask extends AsyncTask<String, Void, Movie[]> {
 
-        private final String LOG_TAG = FetchMovieAsyncTask.class.getSimpleName();
-
-        private  String mApiKey="";
-
-        private  OnTaskCompleted mListener=null;
-
-        public FetchMovieAsyncTask(OnTaskCompleted listener, String apiKey) {
-            super();
-
-            mListener = listener;
-            mApiKey = apiKey;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-        }
-
-        @Override
-        protected Movie[] doInBackground(String... params) {
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-            // Holds data returned from the API
-            String moviesJsonStr = null;
-
-            try {
-                URL url = getApiUrl(params);
-
-                // Start connecting to get JSON
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuilder builder = new StringBuilder();
-
-                if (inputStream == null) {
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Adds '\n' at last line if not already there.
-                    // This supposedly makes it easier to debug.
-                    builder.append(line).append("\n");
-                }
-
-                if (builder.length() == 0) {
-                    // No data found. Nothing more to do here.
-                    return null;
-                }
-
-                moviesJsonStr = builder.toString();
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                return null;
-            } finally {
-                // Tidy up: release url connection and buffered reader
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
-            }
-
-            try {
-                // Make sense of the JSON data
-                return getMoviesDataFromJson(moviesJsonStr);
-            } catch (JSONException e) {
-                Log.e(LOG_TAG, e.getMessage(), e);
-                e.printStackTrace();
-            }
-
-            return null;
-        }
-
-        /**
-         * Extracts data from the JSON object and returns an Array of movie objects.
-         *
-         * @param moviesJsonStr JSON string to be traversed
-         * @return Array of Movie objects
-         * @throws JSONException
-         */
-        private Movie[] getMoviesDataFromJson(String moviesJsonStr) throws JSONException {
-            // JSON tags
-            final String TAG_RESULTS = "results";
-            final String TAG_ORIGINAL_TITLE = "original_title";
-            final String TAG_POSTER_PATH = "poster_path";
-            final String TAG_OVERVIEW = "overview";
-            final String TAG_VOTE_AVERAGE = "vote_average";
-            final String TAG_RELEASE_DATE = "release_date";
-
-            // Get the array containing hte movies found
-            JSONObject moviesJson = new JSONObject(moviesJsonStr);
-            JSONArray resultsArray = moviesJson.getJSONArray(TAG_RESULTS);
-
-            // Create array of Movie objects that stores data from the JSON string
-            Movie[] movies = new Movie[resultsArray.length()];
-
-            // Traverse through movies one by one and get data
-            for (int i = 0; i < resultsArray.length(); i++) {
-                // Initialize each object before it can be used
-                movies[i] = new Movie();
-
-                // Object contains all tags we're looking for
-                JSONObject movieInfo = resultsArray.getJSONObject(i);
-
-                // Store data in movie object
-                movies[i].setOriginaltitle(movieInfo.getString(TAG_ORIGINAL_TITLE));
-                movies[i].setPosterPath(movieInfo.getString(TAG_POSTER_PATH));
-                movies[i].setOverview(movieInfo.getString(TAG_OVERVIEW));
-                movies[i].setVoteAverage(movieInfo.getDouble(TAG_VOTE_AVERAGE));
-                movies[i].setReleaseDate(movieInfo.getString(TAG_RELEASE_DATE));
-            }
-
-            return movies;
-        }
-
-        /**
-         * Creates and returns an URL.
-         *
-         * @param parameters Parameters to be used in the API call
-         * @return URL formatted with parameters for the API
-         * @throws MalformedURLException
-         */
-        private URL getApiUrl(String[] parameters) throws MalformedURLException {
-            final String TMDB_BASE_URL = "https://api.themoviedb.org/3/discover/movie?";
-            final String SORT_BY_PARAM = "sort_by";
-            final String API_KEY_PARAM = "api_key";
-            final String ADULT_CONTENT = "include_adult";
-
-            Uri builtUri = Uri.parse(TMDB_BASE_URL).buildUpon()
-                    .appendQueryParameter(SORT_BY_PARAM, parameters[0])
-                    .appendQueryParameter(API_KEY_PARAM, mApiKey)
-                    .appendQueryParameter(ADULT_CONTENT,"false")
-                    .build();
-
-            return new URL(builtUri.toString());
-        }
-
-        @Override
-        protected void onPostExecute(Movie[] movies) {
-            super.onPostExecute(movies);
-            mListener.onFetchMoviesTaskCompleted(movies);
-        }
-    }
 }
--- app/src/main/java/com/thefuturemarketplace/popularmovies/MovieDetailsActivity.java
+++ app/src/main/java/com/thefuturemarketplace/popularmovies/MovieDetailsActivity.java
@@ -14,6 +14,9 @@ import com.thefuturemarketplace.popularmovies.models.Movie;
 
 import java.text.ParseException;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by imKiran
  */
@@ -22,26 +25,20 @@ public class MovieDetailsActivity extends AppCompatActivity{
 
     private final String LOG_TAG = MovieDetailsActivity.class.getSimpleName();
 
-    private ImageView imageViewPoster;
-    private TextView textViewReleaseDateTitle;
-    private TextView textViewReleasedate;
-    private TextView textViewVoteAverageTitle;
-    private TextView textViewVoteAverage;
-    private TextView textViewOriginalTitle;
-    private TextView textViewOverview;
+    @BindView(R.id.imageview_moview) ImageView imageViewPoster;
+    @BindView(R.id.textview_release_date_title) TextView textViewReleaseDateTitle;
+    @BindView(R.id.textview_release_date_example)  TextView textViewReleasedate;
+    @BindView(R.id.textview_vote_average_title)  TextView textViewVoteAverageTitle;
+    @BindView(R.id.textview_vote_average_example) TextView textViewVoteAverage;
+    @BindView(R.id.textview_original_title_example)  TextView textViewOriginalTitle;
+    @BindView(R.id.textview_overview_example)  TextView textViewOverview;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
 
-        imageViewPoster = (ImageView)findViewById(R.id.imageview_moview);
-        textViewReleaseDateTitle = (TextView)findViewById(R.id.textview_release_date_title);
-        textViewReleasedate = (TextView)findViewById(R.id.textview_release_date_example);
-        textViewVoteAverageTitle = (TextView)findViewById(R.id.textview_vote_average_title);
-        textViewVoteAverage = (TextView)findViewById(R.id.textview_vote_average_example);
-        textViewOriginalTitle = (TextView)findViewById(R.id.textview_original_title_example);
-        textViewOverview = (TextView)findViewById(R.id.textview_overview_example);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         Movie movie = intent.getParcelableExtra(getString(R.string.parcel_movie));
